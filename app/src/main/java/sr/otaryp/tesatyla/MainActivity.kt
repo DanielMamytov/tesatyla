01ec2b0dd983ad390a27b9eeada70533a28d478d
DanielMamytov <mamytovdaniel.08.02@gmail.com>
2025-10-01 22:18:51 +0600

    Fix splash and onboarding back stack

--- app/src/main/java/sr/otaryp/tesatyla/MainActivity.kt
+++ app/src/main/java/sr/otaryp/tesatyla/MainActivity.kt
@@ -5,6 +5,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +19,25 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        bottomNavigation.setupWithNavController(navController)
+
+        val destinationsWithBottomNav = setOf(
+            R.id.nav_home,
+            R.id.nav_lessons,
+            R.id.nav_articles,
+            R.id.nav_progress,
+            R.id.nav_focus
+        )
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            bottomNavigation.isVisible = destination.id in destinationsWithBottomNav
         }
     }
 }
