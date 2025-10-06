5ec15a35c9d3502631d12d2997533ff84ebaf99b
DanielMamytov <mamytovdaniel.08.02@gmail.com>
2025-10-06 18:10:30 +0600

    Add dots indicator to onboarding view pager

--- app/src/main/java/sr/otaryp/tesatyla/presentation/ui/onboarding/OnBoardingFragment.kt
+++ app/src/main/java/sr/otaryp/tesatyla/presentation/ui/onboarding/OnBoardingFragment.kt
@@ -4,25 +4,24 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.navOptions
-
-
 import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayoutMediator
 import sr.otaryp.tesatyla.R
 import sr.otaryp.tesatyla.data.preferences.LaunchPreferences
 import sr.otaryp.tesatyla.databinding.FragmentOnBoardingBinding
 import sr.otaryp.tesatyla.presentation.ui.lessons.applyVerticalGradient
-import kotlin.math.abs
 
 class OnBoardingFragment : Fragment() {
 
     private var _binding: FragmentOnBoardingBinding? = null
     private val binding get() = _binding!!
     private var pageChangeCallback: ViewPager2.OnPageChangeCallback? = null
+    private var tabMediator: TabLayoutMediator? = null
 
     private val slides by lazy {
         listOf(
@@ -56,7 +55,13 @@ class OnBoardingFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.viewPagerOnboarding.adapter = OnboardingPagerAdapter(slides)
-
+        binding.tabDotsOnboarding.tabIconTint = null
+        tabMediator = TabLayoutMediator(binding.tabDotsOnboarding, binding.viewPagerOnboarding) { tab, _ ->
+            tab.icon = AppCompatResources.getDrawable(requireContext(), R.drawable.onboarding_dot)
+        }.also { mediator ->
+            mediator.attach()
+        }
+        binding.tabDotsOnboarding.post { applyDotsSpacing() }
 
         updateEnterButtonVisibility(0)
 
@@ -88,10 +93,24 @@ class OnBoardingFragment : Fragment() {
         binding.btnEnterKingdom.isVisible = position == slides.lastIndex
     }
 
+    private fun applyDotsSpacing() {
+        val tabStrip = binding.tabDotsOnboarding.getChildAt(0) as? ViewGroup ?: return
+        val margin = resources.getDimensionPixelOffset(R.dimen.onboarding_indicator_margin)
+        for (index in 0 until tabStrip.childCount) {
+            val tabView = tabStrip.getChildAt(index)
+            val params = tabView.layoutParams as? ViewGroup.MarginLayoutParams ?: continue
+            params.marginStart = margin
+            params.marginEnd = margin
+            tabView.layoutParams = params
+        }
+    }
+
     override fun onDestroyView() {
         pageChangeCallback?.let { binding.viewPagerOnboarding.unregisterOnPageChangeCallback(it) }
         binding.viewPagerOnboarding.adapter = null
+        tabMediator?.detach()
         pageChangeCallback = null
+        tabMediator = null
         _binding = null
         super.onDestroyView()
     }
